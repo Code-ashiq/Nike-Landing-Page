7863c4b3ccaafef365d0f28d90528607691ec4a3
Code-ashiq <ashiqmohamed886@gmail.com>
2025-01-20 12:03:59 +0530

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
+  base: "/Nike-Landing-Page",
+  plugins: [react()],  
 })
