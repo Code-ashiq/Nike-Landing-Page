1087021de9b03c171bd4a5cecd6d186da6c1fc95
Code-ashiq <ashiqmohamed886@gmail.com>
2025-07-15 09:57:10 +0530

    fix: optimize images

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Nike-Landing-Page",
+  base: "/Nike-Landing-Page/",
   plugins: [react()],  
 })
